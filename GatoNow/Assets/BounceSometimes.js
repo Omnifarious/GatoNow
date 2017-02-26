2960ac31567e34d1d8abcfbc32db810189be3c91
Eric Hopper <hopper@omnifarious.org>
2017-02-26 12:51:19 -0800

    Added a new NPCTest scene with a bouncing ball on a surface.

---
+++ GatoNow/Assets/BounceSometimes.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var bounceFrequency = 100;
+var bounceForce = 5;
+var sinceLastBounce : float = 0;
+
+function Update () {
+//	var delta : float = Time.deltaTime;
+	sinceLastBounce += Time.deltaTime;
+//	Debug.Log("Delta: " + delta + ", sinceLastBounce: " + sinceLastBounce);
+	if (sinceLastBounce > bounceFrequency) {
+		sinceLastBounce -= bounceFrequency;
+		GetComponent.<Rigidbody>().AddRelativeForce(Vector3.up * bounceForce);
+	}
+}
